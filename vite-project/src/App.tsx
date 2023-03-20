9119d0ad4fc3b60ba963fe154459e9ad75983993
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2023-03-20 11:42:42 +0100

    commit compteur work

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-    const [test, setCount2] = useState(0)
+function increment(this: any){
+    this.setState({compteur : this.state.compteur + 1})
+}
+function App(this: any) {
+    const [count2, setCount2] = useState(0)
   const [count, setCount] = useState(0)
-    const [ for2, setCount3] = useState(0)
+    const [ count3, setCount3] = useState(0)
 
     return (
     <div className="App">
@@ -20,18 +23,18 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => setCount(((count) => count+1))}>
+          Boutton 1 a pour valeur {count+count3}
         </button>
       </div>
         <div className="card">
-            <button onClick={() => setCount2((test) => test + 1)}>
-                count is {test}
+            <button onClick={() => setCount2(((count2) => count2+1))}>
+                Bouttton 2 a pour valeur {count2+count3}
             </button>
         </div>
         <div>
-            <button onClick={() => setCount3((count) => count+1)}>
-
+            <button onClick={() => setCount3((count   ) => count+1)}>
+                Ajoute pour les deux
             </button>
         </div>
     </div>
