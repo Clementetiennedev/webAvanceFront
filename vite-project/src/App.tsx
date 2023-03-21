22f296e80db1c5697a8e70a91943d9947fea467e
clementSIO <116883467+Clementetiennedev@users.noreply.github.com>
2023-03-21 09:49:07 +0100

    commit calculette

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,156 +1,51 @@
-import Wrapper from "./components/wrapper";
-import ButtonBox from "./components/buttonbox";
-import Button from "./components/button";
-import Screen from "./components/screen";
 import React, { useState } from "react";
 
-const btnValues = [
-    ["C", "+-", "%", "/"],
-    [7, 8, 9, "X"],
-    [4, 5, 6, "-"],
-    [1, 2, 3, "+"],
-    [0, ".", "="],
-];
-const toLocaleString = (num: any) =>
-    String(num).replace(/(?<!\..*)(\d)(?=(?:\d{3})+(?:\.|$))/g, "$1 ");
+function Calculator() {
+    const [result, setResult] = useState("");
 
-const removeSpaces = (num: { toString: () => string; }) => num.toString().replace(/\s/g, "");
+    function handleClick(e: any) {
+        setResult(result.concat(e.target.name));
+    }
 
-const App = () => {
-    let [calc, setCalc] = useState({
-        sign: "",
-        num: 0,
-        res: 0,
-    });
-    const numClickHandler = (e) => {
-        e.preventDefault();
-        const value = e.target.innerHTML;
-
-        if (removeSpaces(calc.num).length < 16) {
-            setCalc({
-                ...calc,
-                num:
-                    calc.num === 0 && value === "0"
-                        ? "0"
-                        : removeSpaces(calc.num) % 1 === 0
-                            ? toLocaleString(Number(removeSpaces(calc.num + value)))
-                            : toLocaleString(calc.num + value),
-                res: !calc.sign ? 0 : calc.res,
-            });
+    function calculate() {
+        try {
+            setResult(eval(result).toString());
+        } catch (error) {
+            setResult("Error");
         }
-    };
-    const commaClickHandler = (e: any) => {
-        e.preventDefault();
-        const value = e.target.innerHTML;
+    }
 
-        setCalc({
-            ...calc,
-            num: !calc.num.toString().includes(".") ? calc.num + value : calc.num,
-        });
-    };
-    const signClickHandler = (e: any) => {
-        e.preventDefault();
-        const value = e.target.innerHTML;
+    function clear() {
+        setResult("");
+    }
 
-        setCalc({
-            ...calc,
-            sign: value,
-            res: !calc.res && calc.num ? calc.num : calc.res,
-            num: 0,
-        });
-    };
-    const equalsClickHandler = () => {
-        if (calc.sign && calc.num) {
-            const math = (a: number, b: number, sign: string) =>
-                sign === "+"
-                    ? a + b
-                    : sign === "-"
-                        ? a - b
-                        : sign === "X"
-                            ? a * b
-                            : a / b;
-
-            setCalc({
-                ...calc,
-                res:
-                    calc.num === "0" && calc.sign === "/"
-                        ? "Can't divide with 0"
-                        : toLocaleString(
-                            math(
-                                Number(removeSpaces(calc.res)),
-                                Number(removeSpaces(calc.num)),
-                                calc.sign
-                            )
-                        ),
-                sign: "",
-                num: 0,
-            });
-        }
-    };
-
-    const invertClickHandler = () => {
-        setCalc({
-            ...calc,
-            num: calc.num ? calc.num * -1 : 0,
-            res: calc.res ? calc.res * -1 : 0,
-            sign: "",
-        });
-    };
-    const percentClickHandler = () => {
-        // @ts-ignore
-        let num = calc.num ? parseFloat(calc.num) : 0;
-        // @ts-ignore
-        let res = calc.res ? parseFloat(calc.res) : 0;
-
-        setCalc({
-            ...calc,
-            num: (num /= Math.pow(100, 1)),
-            res: (res /= Math.pow(100, 1)),
-            sign: "",
-        });
-    };
-    const resetClickHandler = () => {
-        setCalc({
-            ...calc,
-            sign: "",
-            num: 0,
-            res: 0,
-        });
-    };
-    // @ts-ignore
     return (
-        <Wrapper>
-            <Screen value={calc.num ? calc.num : calc.res} />
-            <ButtonBox>
-                {
-                    btnValues.flat().map((btn, i) => {
-                        return (
-                            <Button
-                                key={i}
-                                className={btn === "=" ? "equals" : ""}
-                                value={btn}
-                                onClick={
-                                    btn === "C"
-                                        ? resetClickHandler
-                                        : btn === "+-"
-                                            ? invertClickHandler
-                                            : btn === "%"
-                                                ? percentClickHandler
-                                                : btn === "="
-                                                    ? equalsClickHandler
-                                                    : btn === "/" || btn === "X" || btn === "-" || btn === "+"
-                                                        ? signClickHandler
-                                                        : btn === "."
-                                                            ? commaClickHandler
-                                                            : numClickHandler
-                                }
-                            />
-                        );
-                    })
-                }
-            </ButtonBox>
-        </Wrapper>
+        <div className="calculator">
+            <button>CALCULATRICE</button><br/><br/>
+            <form>
+                <input className="input" type="text" value={result}/>
+            </form>
+            <div className="buttons"><br/>
+                <button onClick={clear} className="clear">c</button>
+                <button name="/" onClick={handleClick} className="operator">&divide;</button>
+                <button name="*" onClick={handleClick} className="operator">&times;</button><br/>
+                <button name="7" onClick={handleClick}>7</button>
+                <button name="8" onClick={handleClick}>8</button>
+                <button name="9" onClick={handleClick}>9</button>
+                <button name="-" onClick={handleClick} className="operator">&ndash;</button><br/>
+                <button name="4" onClick={handleClick}>4</button>
+                <button name="5" onClick={handleClick}>5</button>
+                <button name="6" onClick={handleClick}>6</button>
+                <button name="+" onClick={handleClick} className="operator">+</button><br/>
+                <button name="1" onClick={handleClick}>1</button>
+                <button name="2" onClick={handleClick}>2</button>
+                <button name="3" onClick={handleClick}>3</button>
+                <button onClick={calculate} className="equal">=</button><br/>
+                <button name="0" onClick={handleClick}>0</button>
+                <button name="." onClick={handleClick}>.</button>
+            </div>
+        </div>
     );
-};
+}
 
-export default App;
+export default Calculator;
